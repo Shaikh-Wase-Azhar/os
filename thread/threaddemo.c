241ba9eb315801bbfbe25da13f1ffb84eefe1bb8
Shaikh-Wase-Azhar <waseazharmateen@gmail.com>
2020-11-01 15:52:35 +0530

    Thread demo#

---
+++ thread/threaddemo.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<pthread.h>
+#include<unistd.h>
+void *thread(void *arg)
+{
+printf("HI");
+}
+
+int main()
+{
+pthread_t tid;
+pthread_create(&tid, NULL ,thread, NULL);
+sleep(1);
+return 0;
+}
